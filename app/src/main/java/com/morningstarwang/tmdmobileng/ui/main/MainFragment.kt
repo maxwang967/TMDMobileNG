c7205456763f7a884b96e7067a0a5d584c36dd99
morningstarwang <morningstarwang@outlook.com>
2018-12-10 10:46:22 +0800

    version code 23.0 stable version at this branch

--- app/src/main/java/com/morningstarwang/tmdmobileng/ui/main/MainFragment.kt
+++ app/src/main/java/com/morningstarwang/tmdmobileng/ui/main/MainFragment.kt
@@ -20,6 +20,7 @@ import java.net.URL
 
 class MainFragment : BaseFragment() {
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -131,6 +132,7 @@ class MainFragment : BaseFragment() {
                 enableRadios()
             }
         }
+
     }
 
     private fun checkIfAtLeastOneRadioChecked(): Boolean {
